15ac9c64cbb1aa36fd05f985cf1732e139803312
yumiryi <yumi.r.yi@gmail.com>
2025-07-14 19:33:34 -0400

    notehead bounds

--- sandbox.py
+++ sandbox.py
@@ -2,10 +2,12 @@ import logging
 import math
 import json
 import cv2
+import cv2.typing as cvt
 import numpy as np
 import scipy.ndimage
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, Optional, Sequence
+from collections import defaultdict
 from pathlib import Path
 from PIL import Image
 from app.utils.download import download_models, MODELS_DIR
@@ -40,35 +42,6 @@ xml_path = replace_extension(IMAGE_PATH, ".musicxml")
 # GENERAL UTILS
 ########################################
 NDArray = np.ndarray[Any, Any]
-
-
-########################################
-# TESTING UTILS
-########################################
-def write_debug_image(
-    image,
-    name: str,
-    bbox: Optional[tuple[int, int, int, int]] = None,
-    binary_map: Optional[NDArray] = None,
-):
-    Path.cwd().joinpath("debug_images").mkdir(exist_ok=True)
-    filepath = f"debug_images/{name}"
-    if binary_map is not None:
-        status = cv2.imwrite(filepath, binary_map * 255)
-        if status:
-            logger.info("image saved")
-        else:
-            raise Exception("image error")
-    else:
-        vis = image.copy()
-        if bbox:
-            x, y, w, h = bbox
-            cv2.rectangle(vis, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        status = cv2.imwrite(filepath, vis)
-        if status:
-            logger.info("image saved")
-        else:
-            raise Exception("image error")
 
 
 ########################################
@@ -536,11 +509,283 @@ def make_lines_stronger(img: NDArray, kernel_size: tuple[int, int] = (1, 2)) -> 
 ########################################
 # BOUNDING BOX UTILS
 ########################################
-@dataclass
-class 
+def do_polygons_overlap(poly1: cvt.MatLike, poly2: cvt.MatLike) -> bool:
+    # check if any point of one ellipse is inside other ellipse
+    for point in poly1:
+        if cv2.pointPolygonTest(poly2, (float(point[0]), float(point[1])), False) >= 0:
+            return True
+    for point in poly2:
+        if cv2.pointPolygonTest(poly1, (float(point[0]), float(point[1])), False) >= 0:
+            return True
+    return False
 
 
-def create_notehead_bboxes(img: NDArray, min_size: Optional[tuple[int, int]] =(4, 4)) -> list[]
+class Polygon:
+    def __init__(self, polygon: Any):
+        self.polygon = polygon
+
+
+class BoundingBox(Polygon):
+    """
+    A bounding box in the format of (x1, y1, x2, y2)
+    """
+
+    def __init__(self, box: cvt.Rect, contours: cvt.MatLike):
+        self.box = box
+        self.contours = contours
+
+        self.center = (box[0] + box[2]) / 2, (box[1] + box[3]) / 2
+        self.size = (box[2] - box[0], box[3] - box[1])
+        self.rotated_box = (self.center, self.size, 0)
+        super().__init__(cv2.boxPoints(self.rotated_box).astype(np.int64))
+
+
+class AngledBoundingBox(Polygon):
+    def __init__(
+        self,
+        box: cvt.RotatedRect,
+        contours: cvt.MatLike,
+        polygon: Any,
+    ):
+        super().__init__(polygon)
+        self.contours = contours
+
+        angle = box[2]
+        self.box: cvt.RotatedRect  # RotatedRect: ((cx, cy), (w, h), angle)
+        if angle > 135 or angle < -135:
+            angle += -180 if angle > 135 else 180
+            size = (box[1][0], box[1][1])
+        elif angle > 45 or angle < -45:
+            angle += -90 if angle > 45 else 90
+            size = (box[1][1], box[1][0])
+        else:
+            size = (box[1][0], box[1][1])
+        self.box = ((box[0][0], box[0][1]), size, angle)
+
+        self.center = self.box[0]
+        self.size = self.box[1]
+        self.angle = self.box[2]
+
+        self.top_left, self.bottom_left, self.top_right, self.bottom_right = (
+            self.calculate_corners()
+        )
+
+    def calculate_corners(self) -> tuple[
+        tuple[float, float],
+        tuple[float, float],
+        tuple[float, float],
+        tuple[float, float],
+    ]:
+        half_size = np.array([self.size[0] / 2, self.size[1] / 2])
+        half_w, half_h = half_size
+
+        top_left = self.center - half_size
+        bottom_left = self.center + np.array([-half_w, half_h])
+        top_right = self.center + np.array([half_w, -half_h])
+        bottom_right = self.center + half_size
+
+        return (
+            tuple(top_left),
+            tuple(bottom_left),
+            tuple(top_right),
+            tuple(bottom_right),
+        )
+
+    def is_overlapping(self, other: Polygon) -> bool:
+        if not self._can_shapes_possibly_touch(other):
+            return False
+        return do_polygons_overlap(self.polygon, other.polygon)
+
+    def _can_shapes_possibly_touch(self, other: Polygon) -> bool:
+        """
+        A fast check if the two shapes can possibly touch. If this returns False,
+        the two shapes do not touch.
+        If this returns True, the two shapes might touch and further checks are necessary.
+        """
+
+        # get centers and major axes of the rectangles
+        center1, axes1, _ = self.box
+        center2: Sequence[float]
+        axes2: Sequence[float]
+        if isinstance(other, BoundingBox):
+            # rotated_box: tuple[tuple[float, float], tuple[int, int], Literal[0]]
+            center2, axes2, _ = other.rotated_box
+        elif isinstance(other, AngledBoundingBox):
+            # box: tuple[tuple[float, float], tuple[int, int], float]
+            center2, axes2, _ = other.box
+        else:
+            raise ValueError(f"Unknown type {type(other)}")
+        major_axis1 = max(axes1)
+        major_axis2 = max(axes2)
+
+        # calculate distance between centers
+        distance = (
+            (center1[0] - center2[0]) ** 2 + (center1[1] - center2[1]) ** 2
+        ) ** 0.5
+
+        # if distance > sum of major axes, there is no overlap
+        if distance > major_axis1 + major_axis2:
+            return False
+        return True
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, AngledBoundingBox):
+            return self.box == __value.box
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash(self.box)
+
+    def __str__(self) -> str:
+        return str(self.box)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class BoundingEllipse(AngledBoundingBox):
+    def __init__(
+        self,
+        box: cvt.RotatedRect,
+        contours: cvt.MatLike,
+    ):
+        super().__init__(
+            box,
+            contours,
+            cv2.ellipse2Poly(
+                (int(box[0][0]), int(box[0][1])),
+                (int(box[1][0] / 2), int(box[1][1] / 2)),
+                int(box[2]),
+                0,
+                360,
+                1,
+            ),
+        )
+
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent: list[int] = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x: int, y: int) -> None:
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x != root_y:
+            # union by rank to keep tree flat
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
+
+def has_box_valid_size(box: cvt.RotatedRect) -> bool:
+    box_w, box_h = box[1][0], box[1][1]
+    return not math.isnan(box_w) and not math.isnan(box_h) and box_w > 0 and box_h > 0
+
+
+def merge_overlaying_bboxes(
+    boxes: Sequence[AngledBoundingBox],
+) -> list[list[AngledBoundingBox]]:
+    n = len(boxes)
+    uf = UnionFind(n)
+
+    # try to find overlaps and union groups that overlap
+    for i in range(n):
+        for j in range(i + 1, n):
+            if boxes[i].is_overlapping(boxes[j]):
+                uf.union(i, j)
+
+    # create merged groups based on the union-find results
+    merged_groups: dict[int, list[AngledBoundingBox]] = defaultdict(list)
+    for i in range(n):
+        root = uf.find(i)
+        merged_groups[root].append(boxes[i])
+
+    return list(merged_groups.values())
+
+
+def create_bounding_ellipses(
+    img: NDArray,
+    min_size: Optional[tuple[int, int]] = (4, 4),
+) -> list[BoundingEllipse]:
+    """
+    Fits and filters ellipses, merges overlapping ones into groups, and fits
+    one bounding ellipse per group.
+    """
+    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    ellipses = []
+    for contour in contours:
+        MIN_LENGTH_TO_FIT_ELLIPSE = 5
+        if len(contour) < MIN_LENGTH_TO_FIT_ELLIPSE:
+            continue
+
+        fitbox = cv2.fitEllipse(contour)
+        if not has_box_valid_size(fitbox):
+            continue
+
+        ellipse = BoundingEllipse(fitbox, contour)
+        if min_size and (
+            ellipse.size[0] < min_size[0] or ellipse.size[1] < min_size[1]
+        ):
+            continue
+
+        ellipses.append(ellipse)
+
+    # merge overlapping ellipses into groups and fit one ellipse per group
+    groups = merge_overlaying_bboxes(ellipses)
+    result = []
+    for group in groups:
+        complete_contour = np.concatenate([e.contours for e in group])
+        box = cv2.minAreaRect(complete_contour)
+        result.append(BoundingEllipse(box, complete_contour))
+
+    return result
+
+
+########################################
+# TESTING UTILS
+########################################
+def write_debug_image(
+    image,
+    name: str,
+    bbox: Optional[tuple[int, int, int, int]] = None,
+    ellipses: Optional[list[BoundingEllipse]] = None,
+    binary_map: Optional[NDArray] = None,
+):
+    Path.cwd().joinpath("debug_imgs").mkdir(exist_ok=True)
+    filepath = f"debug_imgs/{name}"
+    if binary_map is not None:
+        status = cv2.imwrite(filepath, binary_map * 255)
+        if status:
+            logger.info("image saved")
+        else:
+            raise Exception("image error")
+    else:
+        vis = image.copy()
+
+        if bbox:
+            x, y, w, h = bbox
+            cv2.rectangle(vis, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if ellipses:
+            for ellipse in ellipses:
+                cv2.ellipse(vis, ellipse.box, (0, 255, 0), 2)
+
+        status = cv2.imwrite(filepath, vis)
+        if status:
+            logger.info("image saved")
+        else:
+            raise Exception("image error")
 
 
 ########################################
@@ -564,10 +809,13 @@ predictions.staff = make_lines_stronger(predictions.staff)
 logger.info("Loaded segmentation")
 
 ## predicting symbols
-logger.info("Creating bounding boxes for noteheads")
+logger.info("Creating bounds for noteheads")
+noteheads = create_bounding_ellipses(predictions.notehead)
 
 write_debug_image(image, "staff.png", binary_map=predictions.staff)
 write_debug_image(image, "symbols.png", binary_map=predictions.symbols)
 write_debug_image(image, "stems_rests.png", binary_map=predictions.stems_rests)
 write_debug_image(image, "notehead.png", binary_map=predictions.notehead)
 write_debug_image(image, "clefs_keys.png", binary_map=predictions.clefs_keys)
+
+write_debug_image(image, "ellipses.png", ellipses=noteheads)
