b3be66f6e37612f0aebdc51c10201ef30371a6e4
yumiryi <yumi.r.yi@gmail.com>
2025-09-27 14:43:19 -0400

    redis url

--- web/utils/clients.py
+++ web/utils/clients.py
@@ -3,10 +3,6 @@ from redis import Redis
 from rq import Queue
 from web.utils.constants import REDIS_TIMEOUT
 
-r = Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
-    password=os.getenv("REDIS_PASSWORD"),
-    ssl=os.getenv("REDIS_SSL", "false") == "true",
-)
+
+r = Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 q = Queue(connection=r, default_timeout=REDIS_TIMEOUT)
